b703c7e1b72e5cf7e32f1fa7fd2f39c256b2c16e
Melanie Buck <melaniebuck93@gmail.com>
2020-12-29 21:13:54 +0100

    working on it

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 require('dotenv').config();
 
+const { request } = require('express');
 const express = require('express');
-const path = require('path');
-
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
 
 const SpotifyWebApi = require('spotify-web-api-node');
-const { response } = require('express');
 
 const app = express();
 
-
-app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'hbs');
-
+app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
-const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  });
 
-  spotifyApi
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
   .clientCredentialsGrant()
-  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-  .catch(error => console.log('Something went wrong when retrieving an access token', error));
+  .then((data) => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch((error) =>
+    console.log('Something went wrong when retrieving an access token', error)
+  );
 
 // Our routes go here:
 
@@ -35,20 +35,43 @@ app.get('/', (request, response) => {
   response.render('home');
 });
 
-app.get('/artist-search', (request, response) => {
-  const searchQuery = request.query.q;
-  spotifyApi.searchArtists(searchQuery.toLowerCase())
-  .then(data => {
-    console.log('The received data from the API: ', data.body.artists.items);
-    const response = data.body.artists.items;
-    response.render('artist-search-results', {artist:response
-  })
-  //.catch(err => console.log('The error while searching artists occurred: ', err));
-  });
+app.get('/artist-search', (req, res) => {
+  spotifyApi
+    .searchArtists(req.query.q)
+    .then((data) => {
+      res.render('artist-search-results', { artists: data.body.artists.items });
+    })
+    .catch((err) =>
+      console.log('The error while searching artists occurred: ', err)
+    );
 });
 
-app.get('/*', (request, response) => {
+app.get('/albums/:id', (req, res) => {
+  spotifyApi
+    .getArtistAlbums(req.params.id)
+    .then((data) => {
+      res.render('albums', { albums: data.body.items });
+    })
+    .catch((err) =>
+      console.log('The error while searching albums occurred: ', err)
+    );
+});
+
+app.get('/tracks/:id', (req, res) => {
+    spotifyApi
+      .getAlbumTracks(req.params.id)
+      .then((data) => {
+        res.render('tracks', { tracks: data.body.items });
+      })
+      .catch((err) =>
+        console.log('The error while searching tracks occurred: ', err)
+      );
+  });
+
+app.get('*', (request, response) => {
   response.render('error');
 });
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.listen(3000, () =>
+  console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
+);
